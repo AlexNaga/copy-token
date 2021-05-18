66d6d165b8088fd6e6581206d6fe4e49d954148f
Alex <alex.naga@postnord.com>
2021-05-18 08:07:26 +0200

    fix: don't load styling to speed up pptr

--- src/main.js
+++ src/main.js
@@ -9,6 +9,22 @@ module.exports.main = async (dirName) => {
 
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
+
+  await page.setRequestInterception(true);
+
+  // don't load styling to speed up pptr
+  page.on('request', (req) => {
+    const isCss = req.resourceType() == 'stylesheet';
+    const isFont = req.resourceType() == 'font';
+    const isImage = req.resourceType() == 'image';
+
+    if (isCss || isFont || isImage) {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+
   await page.goto(env.LOGIN_URL);
 
   const loginBtn = await waitForBtnWithTxt({ page, txt: 'Log in' });
@@ -16,7 +32,7 @@ module.exports.main = async (dirName) => {
   await page.type(`[name='password']`, env.PASSWORD);
   await loginBtn.click();
 
-  await page.waitForNavigation({ waitUntil: 'networkidle2' });
+  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
   await page.evaluate(getTopbar, env.TOPBAR_SELECTOR);
 
   const accessToken = await page.evaluate(getAccessToken, env.TOPBAR_SELECTOR);
